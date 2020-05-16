c3574501173def0b183ec69838c9e261b59b0175
David Ferguson <david.ferguson@orchidsoft.com>
2020-05-16 20:09:09 +0100

    Added firebase hosting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,7 @@
       <div class="col-10 col-md-10 col-lg-8 col-xl-7">
         <h4 class="display-4 text-primary mb-2">Meeting Log</h4>
         <p class="lead">
-          This simple app creates meetings, allows people to check in, and
-          picks random users to award giveaways. It's a good example of a
-          Single Page Application which includes connection to a database and
-          routing. It's a practical way to learn
-          <a href="https://vuejs.org/">Vue.js</a>
-          with
-          <a href="https://firebase.google.com">Firebase</a>.
+          This is a playground app to learn firebase integration with VueJs.
         </p>
 
         <router-link
